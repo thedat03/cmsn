22b62c40184f76211af96a052349ecc295618f36
thedat03 <148042696+thedat03@users.noreply.github.com>
2023-12-15 17:05:27 +0700

    cmsn

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Ngọc';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100077663223199';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng báo con';
+const contentCard = 'Tin nhắn bạn đang đọc không có chất béo, không cholesterol và không chứa chất gây nghiện, nó có nhiều đường mà thôi. Dù cho có nhiều đường thế nào đi chăng nữa nó cũng không bao giờ có thể ngọt ngào như bạn – người đang đọc nó.';
+
+
+const giftImage = 'ngoc.jpg';
+const base64 = 'ngoc.jpg';
+const giftImageBase64 = "data:image/png;base64, " + base64;
